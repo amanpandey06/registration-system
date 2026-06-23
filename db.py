8fd71c98bdd5ed4bb2b06a0b6efa26daf860736a
Aman Pandey <amanpandeyy2006@gmail.com>
2026-06-23 05:30:23 +0530

    moved db credentials to ev

--- db.py
+++ db.py
@@ -1,11 +1,15 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def connect_db():
     connection = mysql.connector.connect(
-        host="localhost",
-        user ="root",
-        password ="@man2006ABC",
-        database = "registration_system"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
 
     return connection
